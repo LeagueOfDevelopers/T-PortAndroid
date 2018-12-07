d5aefb638e17de9bb5eb790eb4e703ea8211c46c
Igor <rtviwe@gmail.com>
2018-12-08 00:06:44 +0300

    Kotlin 3.11

--- app/src/main/java/com/lod/rtviwe/tport/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lod/rtviwe/tport/ui/activity/MainActivity.kt
@@ -3,13 +3,7 @@ package com.lod.rtviwe.tport.ui.activity
 import android.os.Bundle
 import androidx.fragment.app.transaction
 import com.lod.rtviwe.tport.R
-import com.lod.rtviwe.tport.ui.fragment.BonusesFragment
-import com.lod.rtviwe.tport.ui.fragment.OrdersFragment
-import com.lod.rtviwe.tport.ui.fragment.ProfileFragment
-import com.lod.rtviwe.tport.ui.fragment.RegisterStepOneFragment
-import com.lod.rtviwe.tport.ui.fragment.RegisterStepThreeFragment
-import com.lod.rtviwe.tport.ui.fragment.RegisterStepTwoFragment
-import com.lod.rtviwe.tport.ui.fragment.SearchFragment
+import com.lod.rtviwe.tport.ui.fragment.*
 import com.lod.rtviwe.tport.ui.listeners.OnRegisterStepOneListener
 import com.lod.rtviwe.tport.ui.listeners.OnRegisterStepThreeListener
 import com.lod.rtviwe.tport.ui.listeners.OnRegisterStepTwoListener
@@ -127,7 +121,7 @@ class MainActivity : BaseActivity(), OnRegisterStepOneListener, OnRegisterStepTw
     }
 
     private fun scrollFragmentToTop() {
-        getCurrentFragment().scrollToTop()
+//        getCurrentFragment().scrollToTop()
     }
 
     private fun getCurrentFragment() = when (currentFragmentId) {
