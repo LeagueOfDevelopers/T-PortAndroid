ed3daeef1c3052b5f69877245362e9af09876b5d
Igor <rtviwe@gmail.com>
2018-12-16 01:00:35 +0300

    Network init

--- app/src/main/java/com/lod/rtviwe/tport/di/AppModule.kt
+++ app/src/main/java/com/lod/rtviwe/tport/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.lod.rtviwe.tport.di
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.lod.rtviwe.tport.TPortApplication
 import com.lod.rtviwe.tport.model.mainscrollitem.SearchItem
+import com.lod.rtviwe.tport.network.RegistrationApi
 import com.lod.rtviwe.tport.viewmodel.*
 import com.redmadrobot.inputmask.helper.Mask
 import org.koin.androidx.viewmodel.experimental.builder.viewModel
 import org.koin.dsl.module.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val mainModule = module {
     viewModel<BonusesViewModel>()
@@ -15,4 +20,12 @@ val mainModule = module {
     viewModel<SearchRoutesViewModel>()
     single { SearchItem("", "", "") }
     single { Mask("+7 ([000]) [000]-[00]-[00]") }
+    single {
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .baseUrl(TPortApplication.URL)
+            .build()
+            .create(RegistrationApi::class.java)
+    }
 }
--- app/src/main/java/com/lod/rtviwe/tport/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/lod/rtviwe/tport/viewmodel/RegisterViewModel.kt
@@ -1,6 +1,8 @@
 package com.lod.rtviwe.tport.viewmodel
 
 import android.app.Application
+import android.util.Log
+import com.lod.rtviwe.tport.network.RegistrationApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -9,6 +11,8 @@ import kotlinx.coroutines.launch
 class RegisterViewModel(app: Application) : BaseViewModel(app) {
 
     private val job = Job()
+    private var code = 0
+    private lateinit var registrationApi: RegistrationApi
 
     private val viewModelScope = CoroutineScope(Dispatchers.IO + job)
 
@@ -17,15 +21,26 @@ class RegisterViewModel(app: Application) : BaseViewModel(app) {
         job.cancel()
     }
 
-    fun sendCodeToPhoneNumber(phoneNumber: Long) {
+    fun sendCodeToPhoneNumber(phoneNumber: Long, serviceApi: RegistrationApi) {
+        registrationApi = serviceApi
         viewModelScope.launch {
-
+            if (registrationApi.sendPhoneNumber(phoneNumber).await().body() == null) {
+                Log.e("RegisterViewModel", "Error while getting code")
+            } else {
+                code = registrationApi.sendPhoneNumber(phoneNumber).await().body()!!
+            }
         }
     }
 
     fun checkCode(code: Int) {
         viewModelScope.launch {
+            val result = registrationApi.sendCode(code).await().isSuccessful
 
+            if (result) {
+                Log.v("RegisterViewModel", "Checking the code")
+            } else {
+                Log.e("RegisterViewModel", "Wrong code probably")
+            }
         }
     }
 
